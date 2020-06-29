3e0193a4f998b6a67ba529ae588f30376a2cbd6e
jamesmart77 <jamesmart77@gmail.com>
2020-06-29 14:29:45 -0400

    Updates

---
+++ precache-manifest.92f9d54343dc12308187ac074ed31463.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3f386dcd49c2d9c80748f7d1d3ee84",
+    "url": "/bibleApp/index.html"
+  },
+  {
+    "revision": "9b43c391f1c07dd738c1",
+    "url": "/bibleApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9cd2740bd53a6edf339",
+    "url": "/bibleApp/static/js/2.3225c90d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bibleApp/static/js/2.3225c90d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b43c391f1c07dd738c1",
+    "url": "/bibleApp/static/js/main.f22b100f.chunk.js"
+  },
+  {
+    "revision": "59dfc0fb1d97bc20cbb2",
+    "url": "/bibleApp/static/js/runtime-main.211d6da4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bibleApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
